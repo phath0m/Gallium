5adba468eb33e0bcbe1dbc140a7509e391e4f5f6
Viperidae <viper@devlan.dev>
2022-07-10 09:02:04 -0400

    Add license

--- gallium/builtins/code.c
+++ gallium/builtins/code.c
@@ -1,3 +1,21 @@
+/*
+ * code.c - Builtin code type. Code refers to executable Gallium code. This may
+ * or may not be bound to a function.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <gallium/builtins.h>
--- gallium/vm.c
+++ gallium/vm.c
@@ -1,3 +1,20 @@
+/*
+ * vm.c - Gallium bytecode interpreter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,7 +37,6 @@ vm_panic(struct stackframe *frame, int err, const char *fmt, ...)
     struct vm *vm = frame->vm;
     *frame->interrupt_flag_ptr = true;
     vm->vm_errno = err;
-    
     va_start(vlist, fmt);
     vfprintf(stderr, fmt, vlist);
     va_end(vlist);
