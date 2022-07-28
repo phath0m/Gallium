4479332db2645508edcc2c8a7059462ece1fde71
Viperidae <viper@devlan.dev>
2022-07-28 19:28:16 -0400

    update emscripten make config

--- config.emscripten.mk
+++ config.emscripten.mk
@@ -1,12 +1,12 @@
 ###
 ### Configuration for emscripten (Web-assembly target)
 ###
-GALLIUM=bin/gallium.js
+GALLIUM=gallium.js
 GALLIUM_USE_READLINE=no
 
 MY_CC=emcc
 MY_LD=emcc
 
 # Append anything to CFLAGS or LDFLAGS
-MY_CFLAGS=-DGALLIUM_USE_EMSCRIPTEN -DGALLIUM_TARGET_LIBRARY
+MY_CFLAGS=-DGALLIUM_USE_EMSCRIPTEN
 MY_LDFLAGS=-s EXPORTED_RUNTIME_METHODS=["ccall","print","FS"] -s EXPORT_ES6=1 -s USE_ES6_IMPORT_META=0 -s ENVIRONMENT='web' -sFORCE_FILESYSTEM -sSINGLE_FILE 
